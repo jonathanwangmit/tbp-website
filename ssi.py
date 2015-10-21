359b9d1a247a1178dc39c2764be4bfa4146f1a4a
Jonathan Wang <jonathan.wang.mit@gmail.com>
2015-10-21 17:15:27 -0400

    Add SSI server code and instructions

---
+++ ssi.py
@@ -0,0 +1,22 @@
+'''
+Shared code for ssi_server.py and ssi_expander.py.
+'''
+
+import re
+import os.path
+
+def InlineIncludes(path, web_path):
+  """Read a file, expanding <!-- #include --> statements."""
+  def get_include_file_content(x):
+    file_to_read =  x.group(2)
+    if  len(os.path.dirname(web_path)) >2:
+       file_to_read = os.path.join(os.path.dirname(web_path),file_to_read)[1:]
+    if os.path.exists(file_to_read):
+        return open(file_to_read).read()
+    
+  content = open(path).read()
+  content = re.sub(r'<!-- *#include *(virtual|file)=[\'"]([^\'"]+)[\'"] *-->',
+      get_include_file_content,
+      content)
+  return content
+
